35aec24d0952239b88d10d1aeec2993980163312
nithishsingh <nithishsingha@gmail.com>
2024-05-03 23:12:22 +0530

    change in style

--- pages/home.py
+++ pages/home.py
@@ -18,8 +18,8 @@ def show_home():
     }
 
     # Load resources
-    css_file = "styles\main.css"
-    resume_file_path = "assets\\NithishSingh.pdf"
+    css_file = r"styles\main.css"
+    resume_file_path = r"assets\\NithishSingh.pdf"
     profile_pic_path = r"assets\photo.png"
     logo_path = r"assets\atoms.svg"
 
