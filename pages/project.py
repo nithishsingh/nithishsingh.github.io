66703f074abe648895f4db34547b62b85b534825
nithishsingh <nithishsingha@gmail.com>
2024-05-07 20:52:55 +0530

    toggle b1

--- pages/project.py
+++ pages/project.py
@@ -9,8 +9,9 @@ def show_project():
     <iframe width="800" height="600" src="https://app.powerbi.com/view?r=eyJrIjoiMTcxMWQ0NWUtOTg1Mi00YmYwLTg1NTUtMWU0MDc4OGI2ZjA2IiwidCI6ImRmODY3OWNkLWE4MGUtNDVkOC05OWFjLWM4M2VkN2ZmOTVhMCJ9" frameborder="0" allowFullScreen="true"></iframe>
     """
 
+    st.write("Superstore PowerBI Report")
     # Display the embedded Power BI report in Streamlit only when the toggle is True
-    if stoggle("Superstore Analysis",power_bi_embed_code):
+    if stoggle("Toggle Me to View the project",power_bi_embed_code):
         st.markdown(power_bi_embed_code, unsafe_allow_html=True)
 
 
